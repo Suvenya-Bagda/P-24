a7e3ff089a032fa2a3cb6d8353231201ff7dd2bd
Suvenya-Bagda <81688661+Suvenya-Bagda@users.noreply.github.com>
2021-08-21 15:24:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var paper,dustbin,ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+  paper=new Paper(100,100);
+  dustbin=new Dustbin(810);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
